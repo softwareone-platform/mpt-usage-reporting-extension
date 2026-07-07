253df8fa4fce027038762dd0e027e773add8ab0a
Albert Solà <albert.sola@softwareone.com>
2026-07-07 16:40:53 +0100

    chore: remove unused routers and frontend before deployment (#51)

--- backend/tests/flows/pipelines/test_purchase.py
+++
@@ -1,9 +0,0 @@
-from mpt_usage_reporting_extension.flows.pipelines.purchase import PurchasePipeline
-from mpt_usage_reporting_extension.flows.steps.log_order import LogOrderStep
-
-
-def test_purchase():
-    result = PurchasePipeline().steps
-
-    assert len(result) == 1
-    assert isinstance(result[0], LogOrderStep) is True
